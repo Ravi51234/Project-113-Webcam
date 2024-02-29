f2d40bdd4691ea623264669ed974f12aa0502212
Ravi51234 <140091754+Ravi51234@users.noreply.github.com>
2024-02-29 18:36:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 80, 80, 450, 300);
+    fill("green");
+    noFill();
+    stroke("green");
+    strokeWeight(10);
+    rect(40, 40, 550, 400);
+    fill("red");
+    stroke("red");
+    circle(40, 40, 50);
+    circle(580, 40, 50);
+    circle(40, 440, 50);
+    circle(590, 440, 50);
+}
+
+function save(){
+
+    save("Image_With_Shapes.png");
+}
